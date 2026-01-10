5e9a2624b4fd2bdd7a214cfc11630c388ef682f4
Lkhan05 <m.elxan2005@gmail.com>
2026-01-10 14:24:58 +0400

    dashboard togle side bar

--- src/main/java/com/example/carrent/dtos/car/CarBlogDto.java
+++ src/main/java/com/example/carrent/dtos/car/CarBlogDto.java
@@ -1,13 +1,5 @@
 package com.example.carrent.dtos.car;
 
-//import lombok.Data;
-//@Data
-//public class CarBlogDto {
-//    private Long id;
-//    private String brand;
-//    private String model;
-//    private String imageUrl;
-//}
 
 public record CarBlogDto(
         Long id,
--- src/main/resources/static/dashboard/js/sb-admin-2.js
+++ src/main/resources/static/dashboard/js/sb-admin-2.js
@@ -1,50 +1,69 @@
 (function($) {
-  "use strict"; // Start of use strict
+  "use strict";
 
-  // Toggle the side navigation
-  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
-    $("body").toggleClass("sidebar-toggled");
-    $(".sidebar").toggleClass("toggled");
-    if ($(".sidebar").hasClass("toggled")) {
+  // --- KRİTİK HİSSƏ: Səhifə yüklənən kimi sıçrayışı önləmək üçün ---
+  // jQuery-nin tam yüklənməsini gözləmədən yaddaşı yoxlayırıq
+  if (localStorage.getItem('sidebar-state') === 'toggled') {
+    document.documentElement.classList.add('sidebar-toggled');
+    // Sidebar elementi HTML-də görünən kimi dərhal klası əlavə edirik
+    var checkExist = setInterval(function() {
+       var sidebar = document.querySelector('.sidebar');
+       if (sidebar) {
+          sidebar.classList.add('toggled');
+          clearInterval(checkExist);
+       }
+    }, 1); // 1ms-lik yoxlanış
+  }
+
+  // Normal jQuery funksiyaları başlayır
+  $(function() {
+    // Əgər yaddaşda varsa, Bootstrap-ın collapse funksiyalarını təmizləyirik
+    if (localStorage.getItem('sidebar-state') === 'toggled') {
       $('.sidebar .collapse').collapse('hide');
-    };
+    }
+
+    // Toggle düymələri
+    $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+      $("body").toggleClass("sidebar-toggled");
+      $(".sidebar").toggleClass("toggled");
+
+      if ($(".sidebar").hasClass("toggled")) {
+        localStorage.setItem('sidebar-state', 'toggled');
+        $('.sidebar .collapse').collapse('hide');
+      } else {
+        localStorage.setItem('sidebar-state', 'expanded');
+      }
+    });
   });
 
-  // Close any open menu accordions when window is resized below 768px
+  // Digər orijinal SB Admin kodları (Resize, Scroll və s.)
   $(window).resize(function() {
     if ($(window).width() < 768) {
       $('.sidebar .collapse').collapse('hide');
-    };
-    
-    // Toggle the side navigation when window is resized below 480px
+    }
     if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
       $("body").addClass("sidebar-toggled");
       $(".sidebar").addClass("toggled");
       $('.sidebar .collapse').collapse('hide');
-    };
+    }
   });
 
-  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
   $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
     if ($(window).width() > 768) {
-      var e0 = e.originalEvent,
-        delta = e0.wheelDelta || -e0.detail;
+      var e0 = e.originalEvent, delta = e0.wheelDelta || -e0.detail;
       this.scrollTop += (delta < 0 ? 1 : -1) * 30;
       e.preventDefault();
     }
   });
 
-  // Scroll to top button appear
   $(document).on('scroll', function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
+    if ($(this).scrollTop() > 100) {
       $('.scroll-to-top').fadeIn();
     } else {
       $('.scroll-to-top').fadeOut();
     }
   });
 
-  // Smooth scrolling using jQuery easing
   $(document).on('click', 'a.scroll-to-top', function(e) {
     var $anchor = $(this);
     $('html, body').stop().animate({
@@ -53,4 +72,4 @@
     e.preventDefault();
   });
 
-})(jQuery); // End of use strict
+})(jQuery);
