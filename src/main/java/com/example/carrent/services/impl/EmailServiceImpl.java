8f1a64e420d7444a1cb9dc071817f74a9bd2900d
Emin <90997900+n1k61n@users.noreply.github.com>
2026-01-17 15:23:27 +0400

    add Async

--- src/main/java/com/example/carrent/services/impl/EmailServiceImpl.java
+++ src/main/java/com/example/carrent/services/impl/EmailServiceImpl.java
@@ -3,6 +3,7 @@ import com.example.carrent.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void sendOtpEmail(String toEmail, String otp) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("eminelxanoglu@gmail.com");
