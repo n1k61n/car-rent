41a185d1a254a320712635bbd1193c08c5a73f9a
Emin <90997900+n1k61n@users.noreply.github.com>
2026-01-17 16:35:27 +0400

    add executor

--- src/main/java/com/example/carrent/CarRentApplication.java
+++ src/main/java/com/example/carrent/CarRentApplication.java
@@ -2,7 +2,11 @@ package com.example.carrent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @EnableAsync
 @SpringBootApplication
@@ -10,4 +14,16 @@ public class CarRentApplication {
 	 public static void main(String[] args) {
 		SpringApplication.run(CarRentApplication.class, args);
 	}
+
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("EmailSender-");
+        executor.initialize();
+        return executor;
+    }
 }
