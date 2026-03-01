97a90e59767b4dbca1714dfefebe53a6e479e1ca
Emin <90997900+n1k61n@users.noreply.github.com>
2026-03-01 17:59:19 +0400

    email change

--- src/main/java/com/example/carrent/services/EmailService.java
+++ src/main/java/com/example/carrent/services/EmailService.java
@@ -1,6 +1,7 @@
 package com.example.carrent.services;
 
 public interface EmailService {
+    void sendEmail(String to, String subject, String body);
     void sendOtpEmail(String toEmail, String otp);
     void sendNewPasswordEmail(String toEmail, String newPassword);
 }
